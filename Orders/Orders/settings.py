a3d5be489de6e0a20c69eedafb61eaa2eb85f4b6
iPavliv <iryna.pavliv96@gmail.com>
2020-11-09 18:21:32 +0200

    Add Order and OrderItem models.

--- Orders/Orders/settings.py
+++ Orders/Orders/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'SoldGoods.apps.SoldGoodsConfig',
+    'SoldGoods.apps.SoldgoodsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Orders/SoldGoods/models.py
+++ Orders/SoldGoods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    number = models.IntegerField()
+    created_date = models.DateTimeField()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=50)
+    product_price = models.DecimalField(max_digits=2, decimal_places=2)
+    amount = models.IntegerField()
